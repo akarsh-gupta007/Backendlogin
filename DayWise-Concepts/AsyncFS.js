a49e11db7f4e5bba7a8530a87964e86b58bb7df3
Akarshgu <akarshgupta705415@gmail.com>
2023-03-06 12:14:25 +0530

    ok v

--- DayWise-Concepts/AsyncFS.js
+++
@@ -1,7 +0,0 @@
-        const fs = require("fs")
-
-        fs.writeFile("read.txt", " this is the another msg by using asyncmethod",
-            (err) => {
-                console.log("file is created")
-            }
-        )
