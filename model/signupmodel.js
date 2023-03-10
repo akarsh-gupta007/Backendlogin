a0d570076198503d7003189a2489da877b8fa681
Akarshgu <akarshgupta705415@gmail.com>
2023-03-10 14:44:12 +0530

    okkkkk

--- model/signupmodel.js
+++ model/signupmodel.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose")
 const userSchema =  mongoose.Schema({
     firstName: {
         type: String,
-        required: true
+        required: true,
+        uppercase:true
     },
     lastName: {
         type: String,
-        required: true
+        required: true,
+        uppercase:true
     },
     email: {
         type: String,
